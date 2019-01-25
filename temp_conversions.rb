7a5d7c84b3f5abaa1d38a695017d61277223b67e
Imane Bendou <ibendou@andrew.cmu.edu>
2019-01-25 16:24:55 +0000

    Added Celsius conversion and measure parameter

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,7 +1,11 @@
-def convert(temp)
+def convert(temp, measure="F")
     return "Temperature must be an integer" unless temp.class == Integer
-    return "Temperature below Absolute Zero" if temp < -474 
-    5 * (temp - 32)/9
+    return "Temperature below Absolute Zero" if temp < -474
+    if measure.match("C")
+        (temp*9/5) + 32
+    else    
+        5 * (temp - 32)/9
+    end
 end
 
 
@@ -11,3 +15,8 @@ puts convert(50)
 puts convert(212)
 puts convert("zero")
 puts convert(-500)
+
+puts convert(0, "C")
+puts convert(10, "C")
+puts convert(100, "C")
+puts convert(-280, "C")
