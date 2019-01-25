724d47cc77f64a08ba319c4c75d061c64456cdc0
Imane Bendou <ibendou@andrew.cmu.edu>
2019-01-25 16:39:20 +0000

    Added absolute zero method and fixed the conditions

--- temp_conversions.rb
+++ temp_conversions.rb
@@ -1,6 +1,6 @@
 def convert(temp, measure="F")
     return "Temperature must be an integer" unless temp.class == Integer
-    return "Temperature below Absolute Zero" if temp < -474
+    return "Temperature below Absolute Zero" if below_absolute_zero?(temp, measure)
     if measure.match("C")
         (temp*9/5) + 32
     else    
@@ -8,6 +8,9 @@ def convert(temp, measure="F")
     end
 end
 
+def below_absolute_zero?(temp, measure)
+    (temp < -454 and measure == "F") or (temp < -270 and measure == "C")
+end
 
 #Test Cases
 puts convert(32)          
